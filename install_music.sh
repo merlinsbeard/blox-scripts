798fa3ee1c1bf4530a59806751e17bde3d0bced7
bj <bjpaat01@gmail.com>
2014-06-02 13:35:19 +0800

    made mpd user service

--- install_music.sh
+++ install_music.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sudo apt-get install mpd ncmpcpp cantata
-sudo cp mpd.conf2 /etc/mpd.conf
+#sudo cp mpd.conf2 /etc/mpd.conf
+cp mpdconf ~/.mpdconf
 sudo killall mpd
 sudo service mpd stop
-sudo service mpd start
+sudo update-rc.d mpd disable
+mpd
