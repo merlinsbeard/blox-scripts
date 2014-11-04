9c15b69f07ce43c3dc62ff98fa2a018c07063e3d
bj paat <bjpaat01@gmail.com>
2014-11-04 23:09:40 +0800

    improved install_blox-user-homepage.sh

--- install_blox-user-homepage.sh
+++ install_blox-user-homepage.sh
@@ -8,6 +8,13 @@ else
     echo "running as sudo"
 fi
 
+echo "Now checking if blox.conf exists"
+if [! -f /etc/init/blox.conf ]
+then
+    echo "Copying blox.conf"
+    cp blox.conf /etc/init/
+fi
+
 echo "Now Downloading blox user homepage"
 git clone https://github.com/merlinsbeard/blox-user-homepage.git
 echo "Moving blox-user-homepage.git"
@@ -16,12 +23,20 @@ echo "changing mod of blox-user-homepage to blox and 775"
 cd /var/www/html/blox/
 echo "current directory" && pwd
 cp blox-user-homepage/skeleton_config.txt blox-user-homepage/config.txt
-chmod -R 775 blox-user-homepage
 
-chown -R blox:blox blox-user-homepage
+chown -R blox:blox /var/www/html/blox
+chmod -R 775 /var/www/html/blox
+
+echo "Installing python dependecies before making virtualenv"
+apt-get install python-dev python-setuptools
+apt-get install libtiff4-dev libjpeg8-dev zlib1g-dev \
+        libfreetype6-dev liblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev python-tk
 
 virtualenv venv
 source venv/bin/activate
 pip install -r blox-user-homepage/requirements.txt
 
+service network-manager restart
 
+cd blox-user-homepage/
+./start.sh
